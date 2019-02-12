2e25e7a3c36e7a585b52d1d92b03af3557c3ce1c
Pim van der Meer <meertrompet@gmail.com>
2019-02-12 19:32:07 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // Boek class
 class Boek {
-  constructor(titel, auteur, aantalAuteurs, paginas, likes) {
-    this.titel = titel;
-    this.auteur = auteur;
-    this.aantalAuteurs = aantalAuteurs;
-    this.paginas = paginas;
-    this.likes = likes;
+  constructor(boekData) {
+    this.titel = boekData.titel || 'titel';
+    this.auteur = boekData.auteur || 'auteur';
+    this.aantalAuteurs = boekData.aantalAuteurs || 1;
+    this.paginas = boekData.paginas || 0;
+    this.likes = boekData.likes || 0;
   }
   dislike() { // dislike: haalt 1 van het totale aantal likes af. Aantal likes mag niet minder dan 0
     if (this.likes > 0) {
@@ -16,36 +16,31 @@ class Boek {
     this.likes++;
   }
   toString() { // toString: geeft een string representatie van een boek.
-      //dit moet ook met een ternary operator kunnen maar zie even niet hoe
-
-      if (this.aantalAuteurs > 1) {
-        return (`${this.titel} has been written by ${this.auteur} and ${this.aantalAuteurs - 1} other(s). It has ${this.paginas} pages and ${this.likes} like(s).`)
-      } else {
-        return (` ${this.titel} has been written by ${this.auteur}. It has ${this.paginas} pages and ${this.likes} like(s).`)
-      }
+      return (`${this.titel} has been written by ${this.auteur} ${this.aantalAuteurs > 1 ?
+              `and ${this.aantalAuteurs - 1} other(s). It has ${this.paginas} pages and ${this.likes} like(s).` :
+              `It has ${this.paginas} pages and ${this.likes} like(s).`}`);
   }
 };
 
 
 // **************************************************
 let boekenLijst = [];
-boekenLijst.push(new Boek('JavaScript: The Good Parts', 'Douglas Crockford', 1, 234, 10));
-boekenLijst.push(new Boek('JavaScript for Cats', 'Max Ogden', 1, 201, 10));
-boekenLijst.push(new Boek('Eloquent JavaScript', 'Marijn Haverbeke', 1, 262, 1));
-boekenLijst.push(new Boek('The Magic Mountain','Thomas Mann', 1, 235, 32));
-boekenLijst.push(new Boek('Things Fall Apart','Chinua Achebe', 1, 938, 40));
-boekenLijst.push(new Boek('Het bureau', 'J.J. Voskuil',  1, 48390, 10));
-boekenLijst.push(new Boek('ES6 the new thing', 'Pim van der Meer',  2, 332, 539));
-
+boekenLijst.push(new Boek({titel: 'JavaScript: The Good Parts', auteur:'Douglas Crockford', aantalAuteurs: 1, paginas: 234, likes: 10}));
+boekenLijst.push(new Boek({titel: 'JavaScript for Cats', auteur: 'Max Ogden', aantalAuteurs: 1, paginas: 201, likes: 10}));
+boekenLijst.push(new Boek({titel: 'Eloquent JavaScript', auteur: 'Marijn Haverbeke', aantalAuteurs: 1, paginas: 262, likes: 10}));
+boekenLijst.push(new Boek({titel: 'The Magic Mountain', auteur: 'Thomas Mann', aantalAuteurs: 1, paginas: 235, likes: 32}));
+boekenLijst.push(new Boek({titel: 'Things Fall Apart', auteur: 'Chinua Achebe', aantalAuteurs: 1, paginas: 938, likes: 40}));
+boekenLijst.push(new Boek({titel: 'Het bureau', auteur: 'J.J. Voskuil', aantalAuteurs: 1, paginas: 48390, likes: 10}));
+boekenLijst.push(new Boek({titel: 'ES6 is the new thing', auteur: 'Pim van der Meer', aantalAuteurs: 2, paginas: 332, likes: 539}));
+boekenLijst.push(new Boek({titel: 'ES6 het is het', auteur: 'Pim van der Meer', likes: 539}));
 
 // Nu wordt, m.b.v. deze boekenlijst een <select> gevuld.
-let kiesBoek = document.getElementById('kiesBoek');
+var kiesBoek = document.getElementById('kiesBoek');
 for (var i=0; i<boekenLijst.length; i++) {
     kiesBoek[kiesBoek.length] = new Option(boekenLijst[i].titel, i);
-}
+};
 
 // Hier voegen we twee javascript event listeners toe aan de elementen met IDs 'dislike' en 'like'.
-// Dit hoef je op dit punt in het traject nog niet zelf te kunnen.
 document.getElementById('dislike').addEventListener('click', dislikeBoek);
 document.getElementById('like').addEventListener('click', likeBoek);
 
@@ -57,7 +52,7 @@ function dislikeBoek(oEvent) {
 
     boekenLijst[gekozenBoek].dislike();
     alert(boekenLijst[gekozenBoek].toString());
-}
+};
 
 function likeBoek(oEvent) {
     oEvent.preventDefault();
